930d70bd3312ff0a8109aa475aa9c050307ca724
ta-ku-ya <108854454+ta-ku-ya@users.noreply.github.com>
2022-09-20 13:01:36 +0000

    [Fix]devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :customers
   devise_for :admins
   get  '/top' => 'homes#top'
 
-   get 'items' => 'public/items#index'
+  get 'items' => 'public/items#index'
   namespace :admin do
     resources :items
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_112419) do
+ActiveRecord::Schema.define(version: 2022_09_20_123012) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2022_09_19_112419) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "crat_items", force: :cascade do |t|
